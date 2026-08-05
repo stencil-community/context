c35745d2d7cfe06c543461407d6096b7ca707392
Nikola Svitlica a.k.a TheCelavi <thecelavi@gmail.com>
2026-08-05 12:03:41 +0200

    After build

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,52 +6,19 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface ConsumeExample {
-    }
-    interface ProvideExample {
-        "section": string;
-    }
 }
 declare global {
-    interface HTMLConsumeExampleElement extends Components.ConsumeExample, HTMLStencilElement {
-    }
-    var HTMLConsumeExampleElement: {
-        prototype: HTMLConsumeExampleElement;
-        new (): HTMLConsumeExampleElement;
-    };
-    interface HTMLProvideExampleElement extends Components.ProvideExample, HTMLStencilElement {
-    }
-    var HTMLProvideExampleElement: {
-        prototype: HTMLProvideExampleElement;
-        new (): HTMLProvideExampleElement;
-    };
     interface HTMLElementTagNameMap {
-        "consume-example": HTMLConsumeExampleElement;
-        "provide-example": HTMLProvideExampleElement;
     }
 }
 declare namespace LocalJSX {
-    interface ConsumeExample {
-    }
-    interface ProvideExample {
-        "section"?: string;
-    }
-
-    interface ProvideExampleAttributes {
-        "section": string;
-    }
-
     interface IntrinsicElements {
-        "consume-example": ConsumeExample;
-        "provide-example": Omit<ProvideExample, keyof ProvideExampleAttributes> & { [K in keyof ProvideExample & keyof ProvideExampleAttributes]?: ProvideExample[K] } & { [K in keyof ProvideExample & keyof ProvideExampleAttributes as `attr:${K}`]?: ProvideExampleAttributes[K] } & { [K in keyof ProvideExample & keyof ProvideExampleAttributes as `prop:${K}`]?: ProvideExample[K] };
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "consume-example": LocalJSX.IntrinsicElements["consume-example"] & JSXBase.HTMLAttributes<HTMLConsumeExampleElement>;
-            "provide-example": LocalJSX.IntrinsicElements["provide-example"] & JSXBase.HTMLAttributes<HTMLProvideExampleElement>;
         }
     }
 }
